e22f4a5ca8214e1ca018bd4b8e800976a9599ec1
Liran Tal <liran.tal@gmail.com>
2016-09-28 16:10:27 +0000

    refactoring plugin to perform string match without dependencies can't extend gulp-contains to support different types of callbacks to call upon string matching, had to remove this dependency and implement this functionality

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,5 @@ var gulp = require('gulp');
 var mraudit = require('./index.js');
 
 gulp.task('contains', function() {
-  var opts = ['req.body.', 'eval(', 'child_process.exec('];
-  gulp.src('gulpfile.js').pipe(mraudit(opts));
+  gulp.src('gulpfile.js').pipe(mraudit());
 });
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var contains = require('gulp-contains');
+var through = require('through2');
+var gutil = require('gulp-util');
 
 var defaults = {
   search: [
@@ -9,7 +10,11 @@ var defaults = {
     'child_process.exec(',
     'setTimeout(',
     'setInterval('
-  ]
+  ],
+  onFound: function (string, file, cb) {
+    var error = 'Your file contains "' + string + '", it should not.';
+    cb(new gutil.PluginError('gulp-mraudit', error));
+  }
 };
 
 /**
@@ -26,7 +31,46 @@ function mraudit (opts) {
     };
   }
     
-  return contains(options);
+  return through.obj(function (file, enc, cb) {
+    
+    // Nothing to do with an empty file
+    if (file.isNull()) {
+      cb(null, file);
+      return;
+    }
+
+    // Not processing streams because we need to search the entire content
+    // of the file to find the relevant string
+    if (file.isStream()) {
+      cb(new gutil.PluginError('gulp-mraudit', 'Streaming not supported'));
+      return;
+    }
+
+    // Iterate through the array of strings to find a match,
+    // upon which we should call the handler callback for displaying a console
+    // msg
+    for (var i = 0; i <= options.search.length; i++) {
+      var match = findMatch(file.contents.toString(enc), options.search[i]);
+      if (match) {
+        options.onFound(match, file, cb);
+      }
+    }
+
+    cb(null, file);
+  });
+	
+}
+
+function findMatch(haystack, needle) {
+  if (typeof needle === 'string') {
+    return haystack.indexOf(needle) !== -1 ? needle : false;
+  }
+	
+  if (needle instanceof RegExp) {
+    return haystack.match(needle) ? needle : false;
+  }
+
+  return false;
 }
 
 module.exports = mraudit;
